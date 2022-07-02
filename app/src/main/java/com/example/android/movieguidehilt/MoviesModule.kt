1720e1b0bdf823c192a979f8adc44fd1e5823b73
Efana <efana.usua@wemabank.com>
2022-07-02 15:33:37 +0100

    Unstructured project files added with retrofit and hilt dependencies

---
+++ app/src/main/java/com/example/android/movieguidehilt/MoviesModule.kt
@@ -0,0 +1,39 @@
+package com.example.android.movieguidehilt
+
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object MoviesModule {
+
+    @Provides
+    @Singleton
+    fun provideMoviesRepository(api: MovieGuideApi): MoviesRepository {
+        return MoviesRepositoryImpl(api)
+    }
+
+    @Provides
+    @Singleton
+    fun providesGson(): Gson = GsonBuilder().setLenient().create()
+
+    @Provides
+    @Singleton
+    fun providesMovieGuideApi(gson: Gson): MovieGuideApi {
+        return Retrofit.Builder()
+            .baseUrl("https://api.themoviedb.org")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(MovieGuideApi::class.java)
+    }
+}
+
+
---
+++ app/src/main/java/com/example/android/movieguidehilt/TrendingMoviesResponse.kt
@@ -0,0 +1,9 @@
+package com.example.android.movieguidehilt
+
+data class TrendingMoviesResponse(
+
+    val page : Int?,
+    val results : List<Result>?,
+    val total_pages : Int?,
+    val total_results : Int?
+)
